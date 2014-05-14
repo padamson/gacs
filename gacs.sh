9d2170d868bd988c29238106c4e0f9b3737816c6
paul.adamson.01@gmail.com <paul.adamson.01@gmail.com@a649824e-b015-0aaf-3aec-c07f430d5fe0>
2014-05-14 05:20:02 +0000

    cluster.in and gacs.sh to generate 10 NWChem inputs correctly

--- gacs.sh
+++ gacs.sh
@@ -7,8 +7,6 @@ for i in {1..10}
 do
   tail -n +3 cluster_$i.xyz > cluster_$i.$$
   cat nw_head.txt cluster_$i.$$ nw_tail.txt > cluster_$i.nw
+  rm cluster_$i.xyz cluster_$i.$$
 done
 
-#babel -ixyz $datadir/cluster_1.xyz -ogamin $datadir/cluster_1.inp
-
-rm *.$$
